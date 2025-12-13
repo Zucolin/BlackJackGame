24f3a9c734a567e0a55975710cb160e46d0517d8
Zucolin <vinibr555@gmail.com>
2025-12-13 11:54:27 -0300

    Add project files.

---
+++ BlackJackGame/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace BlackJackGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ganhou = new System.Windows.Forms.Label();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.perdeu1 = new System.Windows.Forms.Label();
+            this.perdeu2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(97, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Continuar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(278, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Parar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(464, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 48);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BLACKJACK";
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(228, 259);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(87, 95);
+            this.num1.TabIndex = 5;
+            this.num1.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(936, 433);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 59);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Parar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Green;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(755, 433);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 59);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Continuar";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // num2
+            // 
+            this.num2.AutoSize = true;
+            this.num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(892, 259);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(87, 95);
+            this.num2.TabIndex = 8;
+            this.num2.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(173, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 58);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Jogador 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label3.Location = new System.Drawing.Point(832, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(248, 58);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Jogador 2";
+            // 
+            // ganhou
+            // 
+            this.ganhou.AutoSize = true;
+            this.ganhou.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ganhou.Location = new System.Drawing.Point(262, 259);
+            this.ganhou.Name = "ganhou";
+            this.ganhou.Size = new System.Drawing.Size(654, 95);
+            this.ganhou.TabIndex = 11;
+            this.ganhou.Text = "Usuario Ganhou";
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.resetbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetbutton.Location = new System.Drawing.Point(515, 433);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(175, 59);
+            this.resetbutton.TabIndex = 12;
+            this.resetbutton.Text = "Jogar Novamente";
+            this.resetbutton.UseVisualStyleBackColor = false;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // perdeu1
+            // 
+            this.perdeu1.AutoSize = true;
+            this.perdeu1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perdeu1.ForeColor = System.Drawing.Color.Red;
+            this.perdeu1.Location = new System.Drawing.Point(210, 199);
+            this.perdeu1.Name = "perdeu1";
+            this.perdeu1.Size = new System.Drawing.Size(90, 29);
+            this.perdeu1.TabIndex = 13;
+            this.perdeu1.Text = "perdeu";
+            this.perdeu1.Visible = false;
+            // 
+            // perdeu2
+            // 
+            this.perdeu2.AutoSize = true;
+            this.perdeu2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perdeu2.ForeColor = System.Drawing.Color.Red;
+            this.perdeu2.Location = new System.Drawing.Point(889, 199);
+            this.perdeu2.Name = "perdeu2";
+            this.perdeu2.Size = new System.Drawing.Size(90, 29);
+            this.perdeu2.TabIndex = 14;
+            this.perdeu2.Text = "perdeu";
+            this.perdeu2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 586);
+            this.Controls.Add(this.perdeu2);
+            this.Controls.Add(this.perdeu1);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.ganhou);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label num2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ganhou;
+        private System.Windows.Forms.Button resetbutton;
+        private System.Windows.Forms.Label perdeu1;
+        private System.Windows.Forms.Label perdeu2;
+    }
+}
+
---
+++ BlackJackGame/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJackGame
+{
+    public partial class Form1 : Form
+    {
+        // ======== VARIÁVEIS DO JOGO ========
+        // Manter nomes próximos aos seus originais
+        int numerosorteio1 = 0; // valor sorteado mais recente (Zucolin)
+        int numerosorteio2 = 0; // acumulador parcial (Zucolin) - usado para somar
+        int numerosorteio3 = 0; // valor sorteado mais recente (Nathália)
+        int numerosorteio4 = 0; // acumulador parcial (Nathália) - usado para somar
+
+        int numerosfinal1 = 0; // total final Zucolin
+        int numerosfinal2 = 0; // total final Nathália
+
+        bool perdeu1Flag = false; // Zucolin passou de 21
+        bool perdeu2Flag = false; // Nathália passou de 21
+
+        bool parouZucolin = false;   // Zucolin apertou "Parar" (button2)
+        bool parouNathalia = false;  // Nathália apertou "Parar" (button3)
+
+        string usuarioganho = "";
+
+        Random r = new Random(); // criar apenas UMA VEZ
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // deixar objetos invisíveis ao iniciar (se existirem no form)
+            ganhou.Visible = false;
+            resetbutton.Visible = false;
+            perdeu1.Visible = false;
+            perdeu2.Visible = false;
+
+            // garantir textos iniciais
+            num1.Text = "0";
+            num2.Text = "0";
+        }
+
+
+        // ======== RESETAR O JOGO ========
+        public void ResetGame()
+        {
+            // tornar controles visíveis novamente
+            button1.Visible = true;  // botão sortear Zucolin
+            button2.Visible = true;  // botão parar Zucolin
+            num1.Visible = true;
+            button3.Visible = true;  // botão parar Nathália
+            button4.Visible = true;  // botão sortear Nathália
+            num2.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+
+            // esconder mensagens de perdeu e ganhou
+            perdeu1.Visible = false;
+            perdeu2.Visible = false;
+            ganhou.Visible = false;
+            resetbutton.Visible = false;
+
+            // reiniciar flags
+            perdeu1Flag = false;
+            perdeu2Flag = false;
+            parouZucolin = false;
+            parouNathalia = false;
+
+            // zerar acumuladores
+            numerosorteio1 = 0;
+            numerosorteio2 = 0;
+            numerosfinal1 = 0;
+
+            numerosorteio3 = 0;
+            numerosorteio4 = 0;
+            numerosfinal2 = 0;
+
+            // mostrar zeros nos labels/textboxes
+            num1.Text = "0";
+            num2.Text = "0";
+
+            // limpar nome do vencedor
+            usuarioganho = "";
+        }
+
+
+        // ======== JOGADOR 1 - ZUCOLIN (Button 1) ========
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // se Zucolin já parou, não pode sortear mais
+            if (parouZucolin) return;
+
+            // gerar um novo número e somar ao acumulador do jogador 1
+            int sorteio = r.Next(1, 11); // 1..9
+            numerosorteio1 = sorteio;
+            numerosorteio2 += sorteio;   // acumulador
+
+            numerosfinal1 = numerosorteio2; // total atual do jogador 1
+
+            num1.Text = numerosfinal1.ToString();
+
+            // verificar condições
+            if (numerosfinal1 == 21)
+            {
+                usuarioganho = "Jogador 1";
+                ganhoujogo();
+                return;
+            }
+
+            if (numerosfinal1 > 21)
+            {
+                perdeu1Flag = true;
+                perdeu1.Visible = true;
+
+                // se um jogador perde, o outro vence automaticamente
+                usuarioganho = "Jogador 2";
+                ganhoujogo();
+                return;
+            }
+
+            // se o outro já perdeu, Zucolin ganha automaticamente
+            if (perdeu2Flag == true)
+            {
+                usuarioganho = "Jogador 1";
+                ganhoujogo();
+                return;
+            }
+        }
+
+
+        // ======== JOGADOR 1 - PARAR (Button 2) ========
+        private void button2_Click(object sender, EventArgs e)
+        {
+            parouZucolin = true;
+
+            // se os dois já pararam, comparar resultados
+            if (parouZucolin && parouNathalia)
+            {
+                CompararResultados();
+            }
+        }
+
+
+
+        // ======== JOGADOR 2 - NATHÁLIA (Button 4) ========
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // se Nathália já parou, não pode sortear mais
+            if (parouNathalia) return;
+
+            // gerar um novo número e somar ao acumulador do jogador 2
+            int sorteio = r.Next(1, 11); // 1..9
+            numerosorteio3 = sorteio;
+            numerosorteio4 += sorteio; // acumulador
+
+            numerosfinal2 = numerosorteio4; // total atual do jogador 2
+
+            num2.Text = numerosfinal2.ToString();
+
+            // verificar condições
+            if (numerosfinal2 == 21)
+            {
+                usuarioganho = "Jogador 2";
+                ganhoujogo();
+                return;
+            }
+
+            if (numerosfinal2 > 21)
+            {
+                perdeu2Flag = true;
+                perdeu2.Visible = true;
+
+                // se um jogador perde, o outro vence automaticamente
+                usuarioganho = "Jogador 1";
+                ganhoujogo();
+                return;
+            }
+
+            // se o outro já perdeu, Nathália ganha automaticamente
+            if (perdeu1Flag == true)
+            {
+                usuarioganho = "Jogador 2";
+                ganhoujogo();
+                return;
+            }
+        }
+
+
+        // ======== JOGADOR 2 - PARAR (Button 3) ========
+        private void button3_Click(object sender, EventArgs e)
+        {
+            parouNathalia = true;
+
+            // se os dois já pararam, comparar resultados
+            if (parouZucolin && parouNathalia)
+            {
+                CompararResultados();
+            }
+        }
+
+
+
+        // ======== COMPARAR QUEM FICOU MAIS PERTO DE 21 ========
+        public void CompararResultados()
+        {
+            // Se ambos passaram de 21, empate/nenhum (já tratamos perdas antes)
+            bool p1Valido = numerosfinal1 <= 21;
+            bool p2Valido = numerosfinal2 <= 21;
+
+            if (!p1Valido && !p2Valido)
+            {
+                usuarioganho = "Empate";
+                ganhoujogo();
+                return;
+            }
+
+            if (p1Valido && !p2Valido)
+            {
+                usuarioganho = "Jogador 1";
+                ganhoujogo();
+                return;
+            }
+
+            if (!p1Valido && p2Valido)
+            {
+                usuarioganho = "Jogador 2";
+                ganhoujogo();
+                return;
+            }
+
+            // Ambos válidos (<=21) -> quem estiver mais perto de 21 vence
+            int diff1 = 21 - numerosfinal1;
+            int diff2 = 21 - numerosfinal2;
+
+            if (diff1 < diff2)
+                usuarioganho = "Jogador 1";
+            else if (diff2 < diff1)
+                usuarioganho = "Jogador 2";
+            else
+                usuarioganho = "Empate";
+
+            ganhoujogo();
+        }
+
+
+
+        // ======== MOSTRAR VITÓRIA (com delay para você ver os números) ========
+        public async void ganhoujogo()
+        {
+            // esperar para permitir ver os números antes de esconder os controles
+            await Task.Delay(1000); // 1000ms = 1 segundo (mude se quiser)
+
+            // esconder controles de jogo
+            button1.Visible = false;
+            button2.Visible = false;
+            num1.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            num2.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            perdeu1.Visible = false;
+            perdeu2.Visible = false;
+
+            // exibir resultado final
+            if (usuarioganho == "Empate")
+                ganhou.Text = "Empate!";
+            else
+                ganhou.Text = usuarioganho + " Ganhou!";
+
+            ganhou.Visible = true;
+            resetbutton.Visible = true;
+        }
+
+
+        // ======== RESET BUTTON CLICK ========
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+    }
+}
